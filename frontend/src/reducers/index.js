f31df81fd65638663ba5f4f548fbb1ddd64cd906
DiegoRenan <renan.ccomp@gmail.com>
2019-08-20 10:14:28 -0300

    create auth Reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
+
 import TicketsReducers from './TicketsReducers'
 import CompaniesReducers from './CompaniesReducers'
 import AlertReducers from './AlertReducers'
+import AuthReducers from '../auth/authReducer'
 
 export default combineReducers({
   ticketsState: TicketsReducers,
   companiesState: CompaniesReducers,
-  alertState: AlertReducers
+  alertState: AlertReducers,
+  auth: AuthReducers
 })
